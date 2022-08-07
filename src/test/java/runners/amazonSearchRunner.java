b77d949f8e7991606f82835d1a9484dd4d5e262e
abdelalies <azoae1@gmail.com>
2022-08-07 12:56:45 -0400

    minor change

--- src/test/java/runners/amazonSearchRunner.java
+++ src/test/java/runners/amazonSearchRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = "step_definitions",
         dryRun = false,
-        tags = "@amazonSearch"
+        tags = "@amazonSearch",
+        plugin = {"pretty", "html:Reports/htmlReport.html", "json:Reports/jsonReport.json"}
 
 )
 
